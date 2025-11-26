710a5983b7078746d119b7063745380d8a886fcb
Victor <visama396@gmail.com>
2025-11-26 12:24:41 +0100

    Updated ET3 DATOS
    Actualizado nombre del proyecto para que se vea reflejado en el index. Arreglada indentación excesiva. Añadidos datos de equipo

--- ET3_Datos_NombreEquipo.js
+++ ET3_Datos_NombreEquipo.js
@@ -2,7 +2,7 @@
 // Sustituir estos valores dummy por los definitivos antes de la entrega.
 const def_datos_NombreEquipo = [
     { entrega: "ET3", nombre: "José Manuel Pérez Suárez", dni: "77014609Y", horas: 50},
-    { entrega: "ET3", nombre: "Nombre Alumno 1", dni: "00000000A", horas: 10 },
+    { entrega: "ET3", nombre: "Víctor Sánchez Martínez", dni: "23953367V", horas: 10 },
     { entrega: "ET3", nombre: "Nombre Alumno 2", dni: "00000000B", horas: 8 },
     { entrega: "ET3", nombre: "Nombre Alumno 3", dni: "00000000C", horas: 12 }
 ];
